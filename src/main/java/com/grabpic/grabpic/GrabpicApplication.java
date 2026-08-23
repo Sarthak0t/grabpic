ecf2905407d9858bd3f68c6b230ad9858effcd82
Sarthak Gupta <sarthakg142006@gmail.com>
2026-08-23 17:13:43 +0530

    chore: complete GrabPic backend setup

---
+++ src/main/java/com/grabpic/grabpic/GrabpicApplication.java
@@ -0,0 +1,13 @@
+package com.grabpic.grabpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabpicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabpicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grabpic/grabpic/GrabpicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabpic.grabpic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabpicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
